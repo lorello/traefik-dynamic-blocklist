03df3e9f3b7daad87bf2b54737bf8960c5b9cead
lorello <lorenzo.salvadorini@opencontent.it>
2021-11-14 20:16:39 +0100

    Added gracefully shutdown

--- main.go
+++ main.go
@@ -3,12 +3,16 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-
+    "os"
+    "os/signal"
+	"time"
 	"github.com/gorilla/mux"
 )
 
@@ -39,17 +43,17 @@ func createIp(w http.ResponseWriter, r *http.Request) {
 	// Convert r.Body into a readable formart
 	reqBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		fmt.Fprintf(w, "Kindly enter data with the event id, title and description only in order to update")
+		fmt.Fprintf(w, "Kindly enter data with the IP id, title and description only in order to update")
 	}
 
 	json.Unmarshal(reqBody, &newIp)
 
-	// Add the newly created event to the array of ips
-	ips = append(ips, newIp)
+	// Add the newly created IP to the array of ips
+	ips[newIp.Ip]=newIp
 
 	// Return the 201 created status code
 	w.WriteHeader(http.StatusCreated)
-	// Return the newly created event
+	// Return the newly created IP
 	json.NewEncoder(w).Encode(newIp)
 }
 
@@ -58,10 +62,10 @@ func isAllowed(w http.ResponseWriter, r *http.Request) {
 
     ip := r.RemoteAddr
     xforward := r.Header.Get("X-Forwarded-For")
-    fmt.Println("IP: ", ip)
-    fmt.Println("X-Forwarded-For: ", xforward)
+    #log.Println("IP: ", ip)
+    #log.Println("X-Forwarded-For: ", xforward)
 
-	// Get the details from an existing event
+	// Get the details from an existing IP
 	// Use the blank identifier to avoid creating a value that will not be used
 	for _, singleIP := range ips {
 		if singleIP.Ip == ip {
@@ -70,7 +74,6 @@ func isAllowed(w http.ResponseWriter, r *http.Request) {
 		if singleIP.Ip == xforward {
 			json.NewEncoder(w).Encode(singleIP)
 		}
-
 	}
 }
 
@@ -79,7 +82,7 @@ func getBlacklistItem(w http.ResponseWriter, r *http.Request) {
 	// Get the ID from the url
 	ipID := mux.Vars(r)["id"]
 
-	// Get the details from an existing event
+	// Get the details from an existing IP
 	// Use the blank identifier to avoid creating a value that will not be used
 	for _, singleIP := range ips {
 		if singleIP.ID == ipID {
@@ -99,7 +102,7 @@ func updateBlacklistItem(w http.ResponseWriter, r *http.Request) {
 	// Convert r.Body into a readable formart
 	reqBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+		fmt.Fprintf(w, "Kindly enter data with the IP title and description only in order to update")
 	}
 
 	json.Unmarshal(reqBody, &updatedEvent)
@@ -113,31 +116,90 @@ func updateBlacklistItem(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func deleteBlacklistItem(w http.ResponseWriter, r *http.Request) {
+/* func deleteBlacklistItem(w http.ResponseWriter, r *http.Request) {
 	// Get the ID from the url
 	ipID := mux.Vars(r)["id"]
 
-	// Get the details from an existing event
+	// Get the details from an existing IP
 	// Use the blank identifier to avoid creating a value that will not be used
 	for i, singleIP := range ips {
 		if singleIP.ID == ipID {
 			ips = append(ips[:i], ips[i+1:]...)
-			fmt.Fprintf(w, "The event with ID %v has been deleted successfully", ipID)
+			fmt.Fprintf(w, "The IP with ID %v has been deleted successfully", ipID)
 		}
 	}
+} */
+
+func loggingMiddleware(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        // Do stuff here
+        log.Println(r.RequestURI)
+        // Call the next handler, which can be another middleware in the chain, or the final handler.
+        next.ServeHTTP(w, r)
+    })
 }
 
 func main() {
+
+	var wait time.Duration
+    flag.DurationVar(&wait, "graceful-timeout", time.Second * 15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+    flag.Parse()
+
 	router := mux.NewRouter().StrictSlash(true)
+
+	router.Use(loggingMiddleware)
+
 	router.HandleFunc("/", homeLink)
 	router.HandleFunc("/check", isAllowed).Methods("GET")
 	router.HandleFunc("/blacklist", createIp).Methods("POST")
 	router.HandleFunc("/blacklist", getBlacklist).Methods("GET")
 	router.HandleFunc("/blacklist/{id}", getBlacklistItem).Methods("GET")
 	router.HandleFunc("/blacklist/{id}", updateBlacklistItem).Methods("PATCH")
-	router.HandleFunc("/blacklist/{id}", deleteBlacklistItem).Methods("DELETE")
-	fmt.Println("Server started, listening on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	//router.HandleFunc("/blacklist/{id}", deleteBlacklistItem).Methods("DELETE")
+
+	srv := &http.Server{
+        Addr:         "0.0.0.0:8000",
+        // Good practice to set timeouts to avoid Slowloris attacks.
+        WriteTimeout: time.Second * 5,
+        ReadTimeout:  time.Second * 5,
+        IdleTimeout:  time.Second * 10,
+        Handler: router, // Pass our instance of gorilla/mux in.
+    }
+
+    // Run our server in a goroutine so that it doesn't block.
+    go func() {
+		log.Println("Server started, listening on port 8080")
+        if err := srv.ListenAndServe(); err != nil {
+            log.Println(err)
+        } else {
+		}
+    }()
+
+	//go func() {
+    //	log.Println(len(ips))
+    //}
+
+    c := make(chan os.Signal, 1)
+    // We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+    // SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+    signal.Notify(c, os.Interrupt)
+
+    // Block until we receive our signal.
+    <-c
+
+    // Create a deadline to wait for.
+    ctx, cancel := context.WithTimeout(context.Background(), wait)
+    defer cancel()
+    // Doesn't block if no connections, but will otherwise wait
+    // until the timeout deadline.
+    srv.Shutdown(ctx)
+    // Optionally, you could run srv.Shutdown in a goroutine and block on
+    // <-ctx.Done() if your application should wait for other services
+    // to finalize based on context cancellation.
+    log.Println("shutting down")
+    os.Exit(0)
+
+
 }
 
 
